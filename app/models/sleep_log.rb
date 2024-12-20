2cb91a0f160435fd85325029ab0d389754be994d
Eore <eore26@gmail.com>
2024-12-20 15:52:55 +0700

    feat: Add sleep log model

---
+++ app/models/sleep_log.rb
@@ -0,0 +1,12 @@
+class SleepLog < ApplicationRecord
+  belongs_to :users
+
+  validates :clock_in, presence: true
+  validate :clock_out, if: -> { clock_out.present? }
+
+  def duration
+    return 0 unless clock_in.present? && clock_out.present?
+
+    clock_out - clock_in
+  end
+end
---
+++ db/migrate/20241220084834_create_sleep_logs.rb
@@ -0,0 +1,10 @@
+class CreateSleepLogs < ActiveRecord::Migration[8.0]
+  def change
+    create_table :sleep_logs do |t|
+      t.timestamp :clock_in
+      t.timestamp :clock_out
+      t.references :users, null: false, foreign_key: true
+
+    end
+  end
+end
