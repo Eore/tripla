27f2c83f3605f2be11101629db1d438e0120f841
Eore <eore26@gmail.com>
2024-12-21 17:43:39 +0700

    test: Add model test for follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,5 +1,13 @@
 class Follow < ApplicationRecord
   belongs_to :user
+  belongs_to :follow, class_name: 'User'
 
   validates :user_id, uniqueness: { scope: :follow_id }
+  validate :cannot_follow_self
+
+  private
+
+  def cannot_follow_self
+    errors.add(:follow_id, "You cannot follow yourself") if user_id == follow_id
+  end
 end
--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class FollowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:miaw)
+    @follow = users(:nyan)
+  end
+
+
+  test "should not save if user_id is empty or follow_id is empty" do
+    follow = Follow.new
+    assert_not follow.save
+  end
+
+  test "should not save if user_id is empty" do
+    follow = Follow.new(follow: @follow)
+    assert_not follow.save
+  end
+
+  test "should not save if follow_id is empty" do
+    follow = Follow.new(user: @user)
+    assert_not follow.save
+  end
+
+  test "should not save if follow itself" do
+    follow = Follow.new(user: @user, follow: @user)
+    assert_not follow.save
+  end
+
+  test "should not save if follow same user again" do
+    Follow.create(user: @user, follow: @follow)
+    follow = Follow.new(user: @user, follow: @follow)
+    assert_not follow.save
+  end
 end
