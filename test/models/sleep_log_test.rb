bd13bb1ae819d1b7df547ca624bd3365ec466ad8
Eore <eore26@gmail.com>
2024-12-21 17:38:13 +0700

    test: Add model test for sleep log

--- test/models/sleep_log_test.rb
+++ test/models/sleep_log_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class SleepLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:miaw)
+  end
+
+  test "should not save if clock_in is empty" do
+    sleep_log = SleepLog.new(user: @user)
+    assert_not sleep_log.save
+  end
+
+  test "should not save if clock_in is empty and clock_out is not empty" do
+    sleep_log = SleepLog.new(clock_out: Time.now, user: @user)
+    assert_not sleep_log.save
+  end
+
+  test "should set duration if clock_in and clock_out is not empty" do
+    sleep_log = SleepLog.create(clock_in: Time.now, clock_out: Time.now + 60, user: @user)
+    assert_equal 60, sleep_log.duration.to_i
+  end
 end
